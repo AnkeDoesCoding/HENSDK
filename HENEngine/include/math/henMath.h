de0b4b88614365e2ab1197d1f99c8da2f27121ac
ADoseOfCode <140978248+AnkeDoesCoding@users.noreply.github.com>
2025-07-17 19:23:40 +0100

    added math

---
+++ HENEngine/include/math/henMath.h
@@ -0,0 +1,69 @@
+#ifndef _HENMATH_H_
+#define _HENMATH_H_
+
+#include "vendor/directxmath/include/DirectXMath.h"
+#include "vendor/directxmath/include/DirectXPackedVector.h"
+
+// the reason i am aliasing directxmath is because it is ugly
+
+namespace hen::math
+{
+    // DirectXMath Aliases
+    // -----------------------------------------------------------------------------------------------------
+
+    using Vector = DirectX::XMVECTOR;
+
+    using Matrix = DirectX::XMMATRIX;
+
+    using Float2 = DirectX::XMFLOAT2;
+
+    using Float3 = DirectX::XMFLOAT3;
+
+    using Float4 = DirectX::XMFLOAT4;
+
+    // VECTOR
+
+    inline Vector Vector3TransformCoord(Vector lookAtVec, Matrix rotMatrix)
+    {
+        return DirectX::XMVector3TransformCoord(lookAtVec, rotMatrix);
+    }
+
+    inline Vector VectorAdd(Vector posVec, Vector lookAtVec)
+    {
+        return DirectX::XMVectorAdd(posVec, lookAtVec);
+    }
+
+    // MATRIX
+
+    inline Matrix IdentityMatrix()
+    {
+        return DirectX::XMMatrixIdentity();
+    }
+
+    inline Matrix MatrixPerspectiveFovLH(float fieldOfView, float aspectRatio, float nearZ, float farZ)
+    {
+        return DirectX::XMMatrixPerspectiveFovLH(fieldOfView, aspectRatio, nearZ, farZ);
+    }
+
+    inline Matrix MatrixOrthographicLH(float viewWidth, float viewHeight, float nearZ, float farZ)
+    {
+        return DirectX::XMMatrixOrthographicLH(viewWidth, viewHeight, nearZ, farZ);
+    }
+
+    inline Matrix MatrixRotationRollPitchYaw(float pitch, float yaw, float roll)
+    {
+        return DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
+    }
+
+    inline Matrix MatrixLookAtLH(Vector posVec, Vector focusVec, Vector upVec)
+    {
+        return DirectX::XMMatrixLookAtLH( posVec,  focusVec,  upVec);
+    }
+
+    inline Matrix MatrixTranspose(Matrix matrix)
+    {
+        return DirectX::XMMatrixTranspose(matrix);
+    }
+}
+
+#endif // !_HENMATH_H_
