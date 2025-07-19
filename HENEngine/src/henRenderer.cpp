b42c9008826b00d5975980e9694c90fa93adcc96
ADoseOfCode <140978248+AnkeDoesCoding@users.noreply.github.com>
2025-07-19 19:20:00 +0100

    multiple cubes

--- HENEngine/src/henRenderer.cpp
+++ HENEngine/src/henRenderer.cpp
@@ -56,6 +56,20 @@ float vertices[] = {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = 
+    {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
 unsigned int VBO;
 unsigned int VAO;
 unsigned int Texture;
@@ -148,7 +162,18 @@ namespace hen::renderer
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, Texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i; 
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            TriangleShader.SetMat4("model", model);
+        
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        
 
         RHC->SwapSwapChain();
     }
