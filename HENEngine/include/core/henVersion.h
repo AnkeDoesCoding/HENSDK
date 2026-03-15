01f74b7cadf22b86de09d215368e7ed210278bea
ADoseOfCode <140978248+AnkeDoesCoding@users.noreply.github.com>
2026-03-15 20:21:37 +0000

    added manual clearing of depth

--- HENEngine/include/core/henVersion.h
+++ HENEngine/include/core/henVersion.h
@@ -7,7 +7,7 @@ namespace hen::version
 {
     constexpr int Major = 0;
     constexpr int Minor = 11;
-    constexpr int Revision = 67;
+    constexpr int Revision = 68;
 
     const std::string Version = std::to_string(Major) + "." + std::to_string(Minor) + "." + std::to_string(Revision);
 }
